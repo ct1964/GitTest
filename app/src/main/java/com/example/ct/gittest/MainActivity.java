c636974e70e0832f230184f13ce43644abca447d
ct1964 <caoting.1964@163.com>
2017-10-03 23:22:30 +0800

    1111

--- app/src/main/java/com/example/ct/gittest/MainActivity.java
+++ app/src/main/java/com/example/ct/gittest/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         testForGitBranch();
         testForConflict();
+        testForConflict2();
         setContentView(R.layout.activity_main);
     }
 
